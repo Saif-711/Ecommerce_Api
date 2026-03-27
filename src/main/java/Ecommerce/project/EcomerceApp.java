c184ea2aab6fcf77772563876d78e8f076b0f4ff
Saif-711 <saifnairat23@gmail.com>
2026-03-27 12:07:14 +0000

    first commit to push repo

---
+++ src/main/java/Ecommerce/project/EcomerceApp.java
@@ -0,0 +1,13 @@
+package Ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomerceApp.class, args);
+    }
+
+}
